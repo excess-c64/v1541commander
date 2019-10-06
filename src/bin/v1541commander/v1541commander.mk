26f7498e8bc0ee3a226648f03d76231d2fe7c453
Felix Palmen <felix@palmen-it.de>
2019-10-06 16:36:12 +0200

    build as GUI app on windows

--- src/bin/v1541commander/v1541commander.mk
+++ src/bin/v1541commander/v1541commander.mk
@@ -6,5 +6,6 @@ v1541commander_PREPROCFLAGS:= -p.
 v1541commander_CXXFLAGS?= -fPIC
 v1541commander_INCLUDES= -I$(v1541commander_PPSRCDIR)
 v1541commander_PKGDEPS:= Qt5Core Qt5Gui	Qt5Widgets 1541img
+v1541commander_win32_LDFLAGS:= -mwindows
 
 $(call binrules, v1541commander)
