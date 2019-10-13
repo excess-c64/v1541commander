9b07e00d45083fc1a7901752622c642896783b71
Felix Palmen <felix@palmen-it.de>
2019-10-13 13:37:43 +0200

    support static win32 cross build with MXE

---
+++ build-static-mxe-win32.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+MXE=/mnt/storage/git/mxe
+TARGET=i686-w64-mingw32.static
+MAKE=gmake
+
+PATH=${MXE}/usr/bin:${PATH} ${MAKE} \
+	CROSS_COMPILE=${TARGET}- \
+	DEFINES=-DQT_STATICPLUGIN \
+	CC=gcc CXX=g++ \
+	PKGCONFIG=${TARGET}-pkg-config \
+	v1541commander_win32_LDFLAGS="-mwindows -L${MXE}/usr/${TARGET}/qt5/plugins/platforms -L${MXE}/usr/${TARGET}/qt5/plugins/styles" \
+	v1541commander_win32_LIBS="qwindows qwindowsvistastyle wtsapi32 Qt5EventDispatcherSupport Qt5ThemeSupport Qt5FontDatabaseSupport Qt5WindowsUIAutomationSupport Qt5PlatformCompositorSupport" \
+	${1:-strip}
+
--- src/bin/v1541commander/main.cpp
+++ src/bin/v1541commander/main.cpp
@@ -1,7 +1,17 @@
 #include "v1541commander.h"
 
+#ifdef QT_STATICPLUGIN
+#include <QtPlugin>
+#endif
+
 int main(int argc, char **argv)
 {
+#ifdef QT_STATICPLUGIN
+#ifdef _WIN32
+    Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+    Q_IMPORT_PLUGIN(QWindowsVistaStylePlugin);
+#endif
+#endif
     V1541Commander commander(argc, argv);
     return commander.exec();
 }
