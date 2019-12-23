d58c671c559751bbc37b7a9809070c986d85207d
Felix Palmen <felix@palmen-it.de>
2019-12-23 22:10:50 +0100

    fix static build for linux

--- build-static-linux64.sh
+++ build-static-linux64.sh
@@ -6,10 +6,12 @@ MAKE=gmake
 QT5STATIC=${LCROSS}/${TARGET}/qt5-static
 
 PKG_CONFIG_PATH=${LCROSS}/${TARGET}/lib/pkgconfig:${QT5STATIC}/lib/pkgconfig \
-	PATH=${LCROSS}/${TARGET}/bin:${PATH} ${MAKE} \
+	PATH=${LCROSS}/${TARGET}/bin:${LCROSS}/bin:${PATH} ${MAKE} \
 	CROSS_COMPILE=${TARGET}- \
 	DEFINES=-DQT_STATICPLUGIN \
 	CC=gcc CXX=g++ \
+	RCC=${QT5STATIC}/bin/rcc \
+	MOC=${QT5STATIC}/bin/moc \
 	v1541commander_posix_LDFLAGS="-static -static-libstdc++ -static-libgcc -L${LCROSS}/lib -L${LCROSS}/lib64 -L${QT5STATIC}/lib -L${QT5STATIC}/plugins/platforms" \
 	v1541commander_posix_LIBS="Qt5Widgets qxcb Qt5XcbQpa Qt5ServiceSupport Qt5ThemeSupport Qt5FontDatabaseSupport xcb-static xcb xkbcommon-x11 xkbcommon Xau Qt5Gui Qt5EdidSupport Qt5DBus Qt5Core png freetype qtharfbuzz fontconfig expat uuid qtpcre2 z pthread dl 1541img c" \
 	${1:-strip}
