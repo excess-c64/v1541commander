9c9f535ac46a2542182a21c6289ed09573322e11
Felix Palmen <felix@palmen-it.de>
2020-01-12 22:28:04 +0100

    install docs

--- src/bin/v1541commander/v1541commander.mk
+++ src/bin/v1541commander/v1541commander.mk
@@ -14,6 +14,7 @@ v1541commander_INCLUDES= -I$(v1541commander_PPSRCDIR)
 v1541commander_PKGDEPS:= Qt5Core Qt5Gui	Qt5Widgets Qt5Network 1541img
 v1541commander_ICONSIZES:= 16x16 32x32 48x48 256x256
 v1541commander_DESKTOPFILE:= v1541commander
+v1541commander_DOCS:= README.md BUILDING.md LICENSE.txt LICENSE-font.txt
 v1541commander_win32_LDFLAGS:= -mwindows
 v1541commander_win32_RES:= windres
 
