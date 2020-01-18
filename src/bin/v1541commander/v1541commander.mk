60d8134ca6d48a5229d5232487e939a1df7cc52f
Felix Palmen <felix@palmen-it.de>
2020-01-18 12:20:55 +0100

    add mime types for supported file types

--- src/bin/v1541commander/v1541commander.mk
+++ src/bin/v1541commander/v1541commander.mk
@@ -16,6 +16,7 @@ v1541commander_PKGDEPS:= Qt5Core Qt5Gui	Qt5Widgets Qt5Network \
 	1541img >= 1.0 1541img < 2
 v1541commander_ICONSIZES:= 16x16 32x32 48x48 256x256
 v1541commander_DESKTOPFILE:= v1541commander
+v1541commander_SHAREDMIMEINFO:= v1541commander
 v1541commander_DOCS:= README.md BUILDING.md LICENSE.txt LICENSE-font.txt
 v1541commander_win32_LDFLAGS:= -mwindows
 v1541commander_win32_RES:= windres
