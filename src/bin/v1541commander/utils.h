b745d47ff773f85bb661c9c6d50df2cfbbaf4e3a
Felix Palmen <felix@palmen-it.de>
2020-03-02 18:14:39 +0100

    win32: avoid runtime widening of mode for qfopen()

--- src/bin/v1541commander/utils.h
+++ src/bin/v1541commander/utils.h
@@ -2,19 +2,22 @@
 #define V1541C_UTILS_H
 
 #ifdef _WIN32
-#include <windows.h>
-#include <QRegularExpression>
+#  include <windows.h>
+#  include <QRegularExpression>
 #endif
 
+#define wstr_int(s) L ## s
+#define wstr(s) wstr_int(s)
+
 #ifdef _WIN32
-#define qfopen(n,m) _wfopen(reinterpret_cast<const wchar_t *>((n).utf16()), \
-        reinterpret_cast<const wchar_t *>(QString((m)).utf16()))
-#define qfname(n) ((n).toUtf8().data())
-#define qfnsan(n) ((n).replace(QRegularExpression("[|.<>/\\\":]"), "_"))
+#  define qfopen(n,m) _wfopen(\
+	reinterpret_cast<const wchar_t *>((n).utf16()), wstr(m))
+#  define qfname(n) ((n).toUtf8().data())
+#  define qfnsan(n) ((n).replace(QRegularExpression("[|.<>/\\\":]"), "_"))
 #else
-#define qfopen(n,m) fopen((n).toLocal8Bit().data(), m)
-#define qfname(n) ((n).toLocal8Bit().data())
-#define qfnsan(n) ((n).replace('/', '_'))
+#  define qfopen(n,m) fopen((n).toLocal8Bit().data(), m)
+#  define qfname(n) ((n).toLocal8Bit().data())
+#  define qfnsan(n) ((n).replace('/', '_'))
 #endif
 
 #endif
